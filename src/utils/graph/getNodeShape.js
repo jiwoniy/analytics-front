54d909a64ccd287dd64fb8e5e8156f8d35c657cc
jiwon <jiwoniy@gmail.com>
2018-07-23 16:51:21 +0900

    wip

--- src/utils/graph/getNodeShape.js
+++ src/utils/graph/getNodeShape.js
@@ -44,81 +44,88 @@ function circleOutputDraghandler ({ context, linkOutput, isCanConnect }) {
 }
 
 function getNodeShape (context, selections, isCanConnect) {
-  function drawCircle (d) {
-    const { input, output } = d
-    const increaseInputValue = (Math.floor(200 / (input * 2))) * 2
-    const startPositionInput = Math.floor(200 / (input * 2))
-    for (let circleIn = 0; circleIn < input; circleIn += 1) {
-      d3Selection.select(this).append('circle')
-        .classed('data-input', true)
-        .attr('id', `input-${circleIn}`)
-        .attr('cx', startPositionInput + (increaseInputValue * circleIn))
-        .attr('cy', 0)
-        .attr('r', 10)
-        .on('mouseover', function (d) {
-          if (context.state.connecting) {
-            d3Selection.select(this).classed('hover', true)
-            context.state.capturedTarget = {
-              ...d,
-              linkInput: {
-                cx: startPositionInput + (increaseInputValue * circleIn),
-                cy: 0,
-                id: this.id,
-                index: circleIn
+  function drawCircle (d, type) {
+    if (type === 'input') {
+      const { input } = d
+      const increaseInputValue = (Math.floor(200 / (input * 2))) * 2
+      const startPositionInput = Math.floor(200 / (input * 2))
+      for (let circleIn = 0; circleIn < input; circleIn += 1) {
+        d3Selection.select(this).append('circle')
+          .classed('data-input', true)
+          .attr('id', `input-${circleIn}`)
+          .attr('cx', startPositionInput + (increaseInputValue * circleIn))
+          .attr('cy', 0)
+          .attr('r', 10)
+          .on('mouseover', function (d) {
+            if (context.state.connecting) {
+              d3Selection.select(this).classed('hover', true)
+              context.state.capturedTarget = {
+                ...d,
+                linkInput: {
+                  cx: startPositionInput + (increaseInputValue * circleIn),
+                  cy: 0,
+                  id: this.id,
+                  index: circleIn
+                }
               }
             }
-          }
-        })
-        .on('mouseout', function (d) {
-          context.state.capturedTarget = null
-          d3Selection.select(this).classed('hover', false)
-        })
-    }
-
-    const increaseOutputValue = (Math.floor(200 / (output * 2))) * 2
-    const startPositionOutput = Math.floor(200 / (output * 2))
-    for (let circleOut = 0; circleOut < output; circleOut += 1) {
-      d3Selection.select(this).append('circle')
-        .classed('data-output', true)
-        .attr('id', `output-${circleOut}`)
-        .attr('cx', startPositionOutput + (increaseOutputValue * circleOut))
-        .attr('cy', 50)
-        .attr('r', 10)
-        .on('mouseover', function (d) {
-          d3Selection.select(this).classed('hover', true)
-        })
-        .on('mouseout', function (d) {
-          d3Selection.select(this).classed('hover', false)
-        })
-        .call(circleOutputDraghandler({
-          context,
-          linkOutput: {
-            cx: startPositionOutput + (increaseOutputValue * circleOut),
-            cy: 50,
-            id: `output-${circleOut}`,
-            index: circleOut
-          },
-          isCanConnect: isCanConnect || checkLinkValidate
-        }))
+          })
+          .on('mouseout', function (d) {
+            context.state.capturedTarget = null
+            d3Selection.select(this).classed('hover', false)
+          })
+      }
+    } else if (type === 'output') {
+      const { output } = d
+      const increaseOutputValue = (Math.floor(200 / (output * 2))) * 2
+      const startPositionOutput = Math.floor(200 / (output * 2))
+      for (let circleOut = 0; circleOut < output; circleOut += 1) {
+        d3Selection.select(this).append('circle')
+          .classed('data-output', true)
+          .attr('id', `output-${circleOut}`)
+          .attr('cx', startPositionOutput + (increaseOutputValue * circleOut))
+          .attr('cy', 50)
+          .attr('r', 10)
+          .on('mouseover', function (d) {
+            d3Selection.select(this).classed('hover', true)
+          })
+          .on('mouseout', function (d) {
+            d3Selection.select(this).classed('hover', false)
+          })
+          .call(circleOutputDraghandler({
+            context,
+            linkOutput: {
+              cx: startPositionOutput + (increaseOutputValue * circleOut),
+              cy: 50,
+              id: `output-${circleOut}`,
+              index: circleOut
+            },
+            isCanConnect: isCanConnect || checkLinkValidate
+          }))
+      }
     }
   }
 
   if (selections.size() === 1) {
     const bindingData = selections.datum()
     // const { input, output } = bindingData
+    drawCircle.call(selections.node(), bindingData, 'input')
     selections.append('rect')
       .attr('width', 200)
       .attr('height', 50)
 
-    drawCircle.call(selections.node(), bindingData)
+    drawCircle.call(selections.node(), bindingData, 'output')
   } else {
     selections
       .call((selection) => {
+        selection.each(function (d) {
+          return drawCircle.call(this, d, 'input')
+        })
         selection.append('rect')
           .attr('width', 200)
           .attr('height', 50)
         selection.each(function (d) {
-          return drawCircle.call(this, d)
+          return drawCircle.call(this, d, 'output')
         })
       })
   }
